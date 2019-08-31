5666a7369f4e915eb7851e4eacdb2681af7b28aa
Stefan Wuthrich <info@exponity.ch>
2019-08-31 03:34:24 +0200

    add mod support

--- db.go
+++ db.go
@@ -158,7 +158,7 @@ type DB struct {
 // Make sure to call Close after you're done.
 func Open(dir string, options ...Option) (*DB, error) {
 	db := &DB{
-		badgerOptions: badger.DefaultOptions,
+		badgerOptions: badger.DefaultOptions(dir),
 		codec:         jsoncodec.Codec{},
 	}
 	for _, option := range options {
